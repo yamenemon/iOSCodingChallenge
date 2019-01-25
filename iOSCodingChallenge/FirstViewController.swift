3f69f571bbfb2e37ae69f989f4064e0ef74545c9
MAC MINI <macmini@MACs-Mac-mini.local>
2019-01-25 21:25:35 +0600

    Initial Commit

---
+++ iOSCodingChallenge/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  iOSCodingChallenge
+//
+//  Created by MAC MINI on 25/1/19.
+//  Copyright © 2019 Emon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
