964af53d8ec6f72bd279470228eb87a7ef14e47e
MAC MINI <macmini@MACs-Mac-mini.local>
2019-01-27 01:27:44 +0600

    try to parse data from base url and added object class

--- iOSCodingChallenge/Controllers/VehicleListViewController.m
+++ iOSCodingChallenge/Controllers/VehicleListViewController.m
@@ -9,13 +9,16 @@
 #import "VehicleListViewController.h"
 #import "iOSCodingChallenge-Swift.h"
 
-@interface VehicleListViewController()
+@interface VehicleListViewController(){
+    NSMutableArray *jsonDataArr;
+}
 @property (weak, nonatomic) IBOutlet UITableView *vehicleTableView;
 @property (strong, nonatomic) NSMutableArray *tableData;
-
 @end
 
 @implementation VehicleListViewController
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -32,8 +35,47 @@
     NSError *error;
     NSString *url_string = [NSString stringWithFormat: @"https://fake-poi-api.mytaxi.com/?p2Lat=53.394655&p1Lon=9.757589&p1Lat=53.694865&p2Lon=10.099891"];
     NSData *data = [NSData dataWithContentsOfURL: [NSURL URLWithString:url_string]];
-    NSMutableArray *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
-    NSLog(@"json: %@", json);
+    id json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+//    NSLog(@"json: %@", json);
+    
+    
+    if(error) { /* JSON was malformed, act appropriately here */ }
+    
+    // the originating poster wants to deal with dictionaries;
+    // assuming you do too then something like this is the first
+    // validation step:
+    if([json isKindOfClass:[NSDictionary class]])
+    {
+        NSArray *results = [json valueForKey:@"poiList"];
+//        NSLog(@"%@",results);
+        jsonDataArr = [[NSMutableArray alloc] init];
+        for (int i = 0; i<results.count; i++) {
+            Vehicle *object = [[Vehicle alloc] init];
+//            object.coordinate.latitude = [[ [results objectAtIndex:i] valueForKey:@"coordinate"] valueForKey:@"latitude"];
+            object.fleetType = [ [results objectAtIndex:i] valueForKey:@"fleetType"];
+            object.heading = [ [results objectAtIndex:i] valueForKey:@"heading"];
+//            object.vehicleId = (int)[results valueForKey:@"id"];
+            [jsonDataArr addObject:object];
+        }
+        NSLog(@"%@",jsonDataArr);
+        
+        /* proceed with results as you like; the assignment to
+         an explicit NSDictionary * is artificial step to get
+         compile-time checking from here on down (and better autocompletion
+         when editing). You could have just made object an NSDictionary *
+         in the first place but stylistically you might prefer to keep
+         the question of type open until it's confirmed */
+        
+    }
+    else
+    {
+        /* there's no guarantee that the outermost object in a JSON
+         packet will be a dictionary; if we get here then it wasn't,
+         so 'object' shouldn't be treated as an NSDictionary; probably
+         you need to report a suitable error condition */
+    }
+    
+    
 }
 
 
@@ -47,12 +89,12 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
     }
     
-    cell.textLabel.text = [_tableData objectAtIndex:indexPath.row];
+    cell.textLabel.text = [jsonDataArr objectAtIndex:indexPath.row];
     return cell;
 }
 
 - (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [_tableData count];
+    return [jsonDataArr count];
 }
 
 
--- iOSCodingChallenge/Model/Vehicle.h
+++ iOSCodingChallenge/Model/Vehicle.h
@@ -14,7 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 @property (retain) Coordinate *coordinate;
 @property (retain) NSString* fleetType;
 @property (retain) NSString* heading;
-@property (assign) double vehicleId;
+@property (assign) int vehicleId;
 @end
 
 NS_ASSUME_NONNULL_END
