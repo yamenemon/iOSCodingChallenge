c0f14036e3f3ad07c752b9880178187f48fcc10e
MAC MINI <macmini@MACs-Mac-mini.local>
2019-01-30 15:18:17 +0600

    designing class for data fetch and ploting annotation

--- iOSCodingChallenge/Controllers/MapViewController.swift
+++ iOSCodingChallenge/Controllers/MapViewController.swift
@@ -10,143 +10,143 @@ import UIKit
 import MapKit
 import MBProgressHUD
 import Alamofire
-
+import CoreLocation
 
 class MapViewController: UIViewController, CLLocationManagerDelegate {
     
     private var cars: [VehicleMapViewModel] = [] 
-    private let locationManager = CLLocationManager()
+    private var locationManager = CLLocationManager()
     private var geofenceRegionCenter: CLLocationCoordinate2D!
     private var tableData: Array<Vehicle>!
+    var previousPosition:CLLocation! = nil
+    var currentPosition: CLLocation! = nil
     @IBOutlet weak var mapView: MKMapView! {
         didSet {
             mapView.showsUserLocation = true
+            mapView.isZoomEnabled = true
         }
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let merc = SphericalMercator()
-        
-        locationManager.requestWhenInUseAuthorization()
-        // Do any additional setup after loading the view.
-        locationManager.stopUpdatingLocation()
-        locationManager.delegate = self
-        locationManager.requestAlwaysAuthorization()
+        determineCurrentLocation()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // 1. status is not determined
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        }
+            // 2. authorization were denied
+        else if CLLocationManager.authorizationStatus() == .denied {
+            print("Location services were previously denied. Please enable location services for this app in Settings.")
+        }
+            // 3. we do have authorization
+        else if CLLocationManager.authorizationStatus() == .authorizedAlways {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    func determineCurrentLocation()
+    {
+        // 2. setup locationManager
+        locationManager.delegate = self;
+        locationManager.distanceFilter = kCLLocationAccuracyNearestTenMeters;
         locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-        locationManager.distanceFilter = 5.0;
-//        locationManager.startUpdatingLocation()
+        locationManager.requestAlwaysAuthorization()
+        
+        
+
+        // 3. setup mapView
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        // 4. setup test data
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
         if #available(iOS 9.0, *) {
             locationManager.allowsBackgroundLocationUpdates = true
-        } else {
-            // Fallback on earlier versions
         }
-        locationManager.pausesLocationUpdatesAutomatically = false
-        
-
+        guard let _: CLLocationCoordinate2D = locationManager.location?.coordinate else {return}
+        let userLocation:CLLocation = locationManager.location!
+        currentPosition = userLocation
     }
-    func startReceivingSignificantLocationChanges() {
-        let authorizationStatus = CLLocationManager.authorizationStatus()
-        if authorizationStatus != .authorizedAlways {
-            // User has not authorized access to location information.
-            return
-        }
-        
-        if !CLLocationManager.significantLocationChangeMonitoringAvailable() {
-            // The service is not available.
-            return
-        }
-        locationManager.delegate = self
-        locationManager.startMonitoringSignificantLocationChanges()
-    }
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//
-////        let geocoder = CLGeocoder()
-////        geocoder.reverseGeocodeLocation(locationManager.location!) { (placemarksArray, error) in
-////
-////            if (placemarksArray?.count)! > 0 {
-////                let placemark = placemarksArray?.first
-////                print(placemark?.locality ?? "Humberg")
-////            }
-////        }
-//        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-//        geofenceRegionCenter = locValue
-//        geofenceRegionCenter?.latitude = (locationManager.location!.coordinate.latitude)
-//        geofenceRegionCenter?.longitude = (locationManager.location!.coordinate.longitude)
-//        print(geofenceRegionCenter!)
-//    }
-}
-
-extension MapViewController: MKMapViewDelegate {
     
-    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-//        if let app = UIApplication.shared.delegate as? AppDelegate, let window = app.window {
-//            MBProgressHUD.showAdded(to: window, animated: true)
-//        }
-        print("calling")
-        DispatchQueue.main.async {
+    // 1. user enter region
+    private func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("enter \(region.identifier)")
+    }
+    
+    // 2. user exit region
+    private func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("exit \(region.identifier)")
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation
+        guard let _: CLLocationCoordinate2D = self.locationManager.location?.coordinate else { return }
+
+        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        
+        if currentPosition == nil {
+            currentPosition = userLocation
+        }
+        let coordinate0 = currentPosition
+        let coordinate1 = userLocation
+        let distanceInMeters = coordinate0!.distance(from: coordinate1)
+        print(distanceInMeters)
+
+        if distanceInMeters > 10 {
+            let regionRadius = distanceInMeters
+            let regions = CLCircularRegion(center: center, radius: regionRadius, identifier: "Your Location")
+            locationManager.startMonitoring(for: regions)
+            
+            let mapViewModel = MapViewModel()
+            
             guard let _: CLLocationCoordinate2D = self.locationManager.location?.coordinate else { return }
             self.centerMap(on: (self.locationManager.location?.coordinate)!)
             
             let northEast = mapView.convert(CGPoint(x: mapView.bounds.width, y: 0), toCoordinateFrom: mapView)
             let southWest = mapView.convert(CGPoint(x: 0, y: mapView.bounds.height), toCoordinateFrom: mapView)
             
-            print("Northest : \(northEast) and Southwest: \(southWest)")
-            
-            let url_string = "https://fake-poi-api.mytaxi.com/?p2Lat=\(northEast.latitude)&p1Lon=\(northEast.longitude)&p1Lat=\(southWest.latitude)&p2Lon=\(southWest.longitude)"
-            
-            let myUrl = NSURL(string: url_string)
-            let request = NSMutableURLRequest(url: myUrl! as URL)
-            request.httpMethod = "GET"
-            
-            // Excute HTTP Request
-            let task = URLSession.shared.dataTask(with: request as URLRequest) {
-                data, response, error in
-                
-                // Check for error
-                if error != nil
-                {
-                    print("error=\(String(describing: error))")
-                    return
-                }
-                
-                // Convert server json response to NSDictionary
-                do {
-                    if let convertedJsonIntoDict = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary {
-                        
-                        // Print out dictionary
-//                        print(convertedJsonIntoDict)
-                        guard let locValue: CLLocationCoordinate2D = self.locationManager.location?.coordinate else { return }
-                        self.geofenceRegionCenter = locValue
-                        self.centerMap(on: self.geofenceRegionCenter)
-                        
-                        
-                        let results = convertedJsonIntoDict["poiList"] as! [NSDictionary]
-                        self.tableData = [Vehicle]()
-                        for object in results{
-                            let vehicle = Vehicle()
-                            let dic = object["coordinate"] as! NSDictionary
-                            vehicle.coordinate.latitude =  dic["latitude"] as! Double
-                            vehicle.coordinate.longitude = dic["longitude"] as! Double
-                            self.tableData.append(vehicle)
-                        }
-                    }
-                } catch let error as NSError {
-                    print(error.localizedDescription)
-                }
+            mapViewModel.loadVehicleWhileUserChangePosition(northEast: northEast, southWest: southWest
+                , onSuccess: { (data) in
+                    self.tableData = (data as! Array<Vehicle>)
+                    self.displayingVehicleInMap()
+            }) { (err) in
+                print("URL is not responding \(String(describing: err))")
             }
-            task.resume()
         }
-        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
+        print(region)
+        if region.notifyOnExit {
+            locationManager.startUpdatingLocation()
+            currentPosition = manager.location
+        }
+        else{
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
+        print("Monitoring failed for region with identifier: \(region!.identifier)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location Manager failed with the following error: \(error)")
+    }
+}
+
+extension MapViewController: MKMapViewDelegate {
+
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+
     }
     
 
     private func centerMap(on coordinate: CLLocationCoordinate2D) {
-//        let regionRadius: CLLocationDistance = 5000
-//        let coordinateRegion = MKCoordinateRegion(center: coordinate,
-//                                                  latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
-//        mapView.setRegion(coordinateRegion, animated: true)
         
         let serialQueue = DispatchQueue(label: "com.test.mySerialQueue")
         serialQueue.sync {
@@ -156,24 +156,15 @@ extension MapViewController: MKMapViewDelegate {
     }
     
     private func displayingVehicleInMap(){
-//        let coordinate = mapView.userLocation.coordinate
-//        guard coordinate.latitude != 0 && coordinate.longitude != 0 else {
-//            return
-//        }
-//        if let app = UIApplication.shared.delegate as? AppDelegate, let window = app.window {
-//            MBProgressHUD.hide(for: window, animated: true)
-//        }
         if self.tableData != nil {
             var coords = NSMutableArray.init() as! [CLLocation]
             for object:Vehicle in self.tableData {
-//                print("\(object.coordinate.latitude) \(object.coordinate.longitude) ")
                 let lat: Double = Double("\(object.coordinate.latitude)")!
                 let lon: Double = Double("\(object.coordinate.longitude)")!
                 coords.append(CLLocation(latitude: lat, longitude:lon))
             }
             addAnnotations(coords: coords)
         }
-        
     }
     func addAnnotations(coords: [CLLocation]){
         DispatchQueue.main.async {
